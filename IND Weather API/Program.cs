63a10a8aeb2532591505a932e675d020ee6df93f
Daniel Gomez <danielg@DBGurusAu.onmicrosoft.com>
2020-11-30 23:13:48 +0800

    #Latest update

--- IND Weather API/Program.cs
+++ IND Weather API/Program.cs
@@ -14,7 +14,7 @@ namespace IND_Weather_API
     class Program
     {
         // Change this path to where your certificate is located
-        private static string certifacatePath = @"C:\Certificates\IND\certificate.pfx";
+        private static string certifacatePath = @"C:\Certificates\IND\UnitedEnergy\unitedenergy.pfx";
 
         // Change this path to your desired output path
         private static string outputPath = @"C:\Users\Admin\source\ImportLogs\IND Weather API Output.txt";
@@ -22,14 +22,39 @@ namespace IND_Weather_API
 
         static void Main(string[] args)
         {
-            string apiUrl = "https://ind.dbgurusnetwork.com.au/api/SiteAPI/rc_au";
+            /* 
+             * Weather API Endpoints
+             * RC AU - https://unitedenergy.rc.weather.api.efd-portal.com.au
+             * RC US - https://unitedenergy.rc.weather.api.efd-portal.com
+             * AU Live - https://unitedenergy.weather.api.efd-portal.com.au
+             * US Live - https://unitedenergy.weather.api.efd-portal.com
+            */
+
+            // Set api endpoint
+            string apiUrl = "https://unitedenergy.weather.api.efd-portal.com.au";
+
+            /* 
+             * Interval can be All or 5
+             * All = All data 
+             * 5 = Only the data on the 5 minute interval
+            */
+
+            // Set interval
             string interval = "All";
-            string unit = "EFD-0111";
-            var startDate = "2020-10-27 12:00";
-            var endDate = "2020-10-29 12:00";
+
+            // Enter unit here
+            string unit = "EFD-0484";
+
+            // Enter start date and end date
+            var startDate = "2020-11-27 12:00";
+            var endDate = "2020-11-30 12:00";
+
             var postedFile = File.OpenRead(certifacatePath);
+
+            // Change date format to yyyy.MM.dd HH:mm
             var uTCDateStart = Convert.ToDateTime(startDate).ToString("yyyy.MM.dd HH:mm");
             var uTCDateEnd = Convert.ToDateTime(endDate).ToString("yyyy.MM.dd HH:mm");
+
             var parameters = new
             {
                 Unit = unit,
@@ -37,14 +62,23 @@ namespace IND_Weather_API
                 UTCDateEnd = uTCDateEnd,
                 Interval = interval
             };
+
+            // Convert object to json
             string json = JsonConvert.SerializeObject(parameters);
+
+            // Post data
             var output = Post(apiUrl, json, ConvertInputStreamToByteArray(postedFile)).Result;
+
             var result = new StringBuilder();
             result.AppendLine("Status Code: " + output["statusCode"]);
             result.AppendLine("\nResponse Body: " + output["responseBody"]);
             result.AppendLine("\nResponse Header: " + output["responseHeader"]);
 
+            // Store ouput in a txt file
             File.WriteAllText(outputPath, result.ToString());
+            
+            Console.WriteLine($"result is stored in {outputPath}");
+            Console.ReadLine();
         }
 
         public static byte[] ConvertInputStreamToByteArray(Stream stream)
@@ -61,16 +95,6 @@ namespace IND_Weather_API
             }
         }
 
-        public static Stream ToStream(string data)
-        {
-            MemoryStream stream = new MemoryStream();
-            StreamWriter writer = new StreamWriter(stream);
-            writer.Write(data);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
-
         public static async Task<Dictionary<string, string>> Post(string apiUrl, string fileContent, byte[] clientCertificatePath = null)
         {
             var result = new Dictionary<string, string>();
@@ -84,12 +108,13 @@ namespace IND_Weather_API
                 if (clientCertificatePath != null)
                 {
                     X509Certificate2 clientCertificate = new X509Certificate2(clientCertificatePath, "", X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
-                    ServicePointManager.Expect100Continue = true;
-                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
                     requestHandler.ClientCertificates.Add(clientCertificate);
-                    client = new HttpClient(requestHandler);
                 }
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
                 client = clientCertificatePath != null ? new HttpClient(requestHandler) : new HttpClient();
+                client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+                client.DefaultRequestHeaders.Add("Keep-Alive", "3600");
                 HttpContent content = new StringContent(fileContent, Encoding.Default, "application/x-www-form-urlencoded");
                 HttpResponseMessage response = await client.PostAsync(apiUrl, content);
                 HttpContent responseContent = response.Content;
